49fceda0f41819490918a1d5e8af04b1001d897b
notl4rd <54897942+notl4rd@users.noreply.github.com>
2026-04-06 11:32:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const keep_alive = require('./keep_alive.js')
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
-    bot.editStatus("dnd", {
-    name: "with my code ✨",   // You can change this to whatever you want
-    type: 0,                   // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 5 = Competing
-  });
-  
   console.error(err); // or your preferred logger
+});
+
+
+bot.on("ready", () => {
+  console.log(`Logged in as ${bot.user.username}`);
+
+  bot.editStatus("idle", {
+    name: "🌊 the sea",   // You can change this to whatever you want
+    type: 2,                   // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 5 = Competing
+  });
+
+  console.log("Status set to Do Not Disturb (DND)");
 });
 
 bot.connect(); // Get the bot to connect to Discord
