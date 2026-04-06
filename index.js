3234b9207231e73417b4171112d9b29b69ee79e7
notl4rd <54897942+notl4rd@users.noreply.github.com>
2026-04-06 11:28:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const keep_alive = require('./keep_alive.js')
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
+    bot.editStatus("dnd", {
+    name: "with my code ✨",   // You can change this to whatever you want
+    type: 0,                   // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 5 = Competing
+  });
+  
   console.error(err); // or your preferred logger
 });
 
